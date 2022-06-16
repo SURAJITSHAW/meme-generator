3eb7b5ded5b67757c0c61e9429a619f52b74a7e5
Surajit <piklu3043@gmail.com>
2022-06-16 16:57:40 +0530

    Markdown done for the site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import reactImg from "./reactjs-icon.png";
 
 function App() {
   return (
-    <h1 className="display-1">React Facts!</h1>
+    <div>
+     <img width="40px" src={ reactImg } alt="react-logo" /> 
+     <h1>Fun facts about React</h1>
+     <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+     </ul>
+    </div>
   );
 }
 
