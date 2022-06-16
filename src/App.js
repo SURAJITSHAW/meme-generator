4128cf86f682a036895f6f209c9645c317604204
Surajit <piklu3043@gmail.com>
2022-06-16 14:00:22 +0530

    Let's code a static site conatining react facts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import waifu from "./images/waifu.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <div className="card">
-        <img src={waifu} className="card-img-top img-fluid w-50" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </div>
+    <h1 className="display-1">React Facts!</h1>
   );
 }
 
