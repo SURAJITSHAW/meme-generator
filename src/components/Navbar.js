a7e07405b48853f4605b49d2cf6c7005565ecbb5
Surajit <piklu3043@gmail.com>
2022-06-17 20:32:39 +0530

    Just a final touch, though I'm not gonna to style it much

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ const NavBar = () => {
     <div className="container p-2">
 
     <header className="d-flex justify-content-between align-items-center shadow-sm pb-2">
-      <nav className="d-flex align-items-center">
-        <img width="40px" src={reactLogo} alt="react-logo" className="img-fluid"/>
-        <h3>ReactFacts</h3>
+      <nav className="d-flex align-items-center justify-content-between" style={{width: "158px"}}>
+        <img width="30px" src={reactLogo} alt="react-logo" className="img-fluid"/>
+        <h3 style={{color: "#61DAFB"}}>ReactFacts</h3>
       </nav>
       <h4 className="d-none d-sm-block">React Course - Project 1</h4>
     </header>
