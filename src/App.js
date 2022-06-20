83c95db5e1e0e53a5c5cf1856bfa34ee7e0ceed9
Surajit <piklu3043@gmail.com>
2022-06-20 09:03:01 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import {CssBaseline } from "@mui/material";
+
+
+
 function App() {
+
   return (
     <CssBaseline>
       <Navbar />
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,31 @@
 import { TextField, Button, Box, Container, Grid } from "@mui/material";
 import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
 import defaultMeme from "../memeimg.png";
+import {useEffect, useState} from "react";
 
-const MainContent = () => {
+let memeDataArray
+
+const fetchMemeImages = async () => {
+  // Where we're fetching data from
+  const response = await fetch("https://api.imgflip.com/get_memes")
+  // We get the API response and receive data in JSON format
+  const data = await response.json()
+  memeDataArray = await data.data.memes[Math.ceil(Math.random() * 100)].url
+
+  console.log(memeDataArray);
+}
+
+const MainContent = (props) => {
+  const [url, setUrl] = useState(defaultMeme)
+
+  useEffect(() => {
+    fetchMemeImages()
+  }, [url]);
+
+  function handleClick() {
+    setUrl(memeDataArray)
+    console.log(memeDataArray);
+  }
   return (
     <>
       <Container maxWidth="md" style={{ marginTop: "50px" }}>
@@ -28,6 +51,7 @@ const MainContent = () => {
           </Grid>
           <Grid item xs={12}>
             <Button
+            onClick={handleClick}
               variant="contained"
               endIcon={<ImageOutlinedIcon />}
               sx={{
@@ -47,7 +71,7 @@ const MainContent = () => {
                 height: "auto",
               }}
               alt="Troll Face"
-              src={defaultMeme}
+              src={url}
             />
           </Grid>
         </Grid>
